d73a88de6f5633c90519f86140dbafeba650acc4
designersakhawat-wq <designersakhawat@gmail.com>
2026-08-26 02:38:33 +0600

    fix: 100% element visibility and eliminate aos opacity 0 bug

--- .next/standalone/src/components/AosProvider.tsx
+++ .next/standalone/src/components/AosProvider.tsx
@@ -1,25 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React from "react";
 
 export function AosProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    AOS.init({
-      duration: 600,
-      once: true,
-      easing: "ease-out-cubic",
-      offset: 20,
-    });
-
-    const timer = setTimeout(() => {
-      AOS.refresh();
-    }, 400);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return <>{children}</>;
 }
+
 
